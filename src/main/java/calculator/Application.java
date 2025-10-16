a9040b6e5971c4c63d02d8cf3f5751f3c3a35726
minbros <pristo24631@gmail.com>
2025-10-16 17:03:02 +0900

    feat: set the default value as 0

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Enter the string you want to add.");
         String input = Console.readLine();
-        System.out.println(input);
+
+        int result = 0;
+        if (!input.isBlank()) {
+
+        }
+
+        System.out.printf("Result: %d%n", result);
     }
 }
