410f5607aa74a785dff77f55f15adff8b7a5ae3e
minbros <pristo24631@gmail.com>
2025-10-16 17:31:00 +0900

    feat: calculate with default separators

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Enter the string you want to add.");
@@ -9,7 +11,20 @@ public class Application {
 
         int result = 0;
         if (!input.isBlank()) {
+            Set<Character> separators = Set.of(',', ':');
+            int value = 0;
 
+            for (int i = 0; i < input.length(); i++) {
+                Character ch = input.charAt(i);
+                if (separators.contains(ch)) {
+                    result = result + value;
+                    value = 0;
+                } else {
+                    value = value * 10 + Integer.parseInt(String.valueOf(ch));
+                }
+            }
+
+            result = result + value;
         }
 
         System.out.printf("Result: %d%n", result);
