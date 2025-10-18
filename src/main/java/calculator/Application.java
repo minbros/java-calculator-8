98ef14c8d294a11a117b20af21bc4a8491c6bff0
minbros <pristo24631@gmail.com>
2025-10-18 19:51:55 +0900

    feat: calculate with custom separators

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,7 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Application {
@@ -11,9 +12,18 @@ public class Application {
 
         int result = 0;
         if (!input.isBlank()) {
-            Set<Character> separators = Set.of(',', ':');
-            int value = 0;
+            Set<Character> separators = new HashSet<>(Set.of(',', ':'));
+            if (input.startsWith("//")) {
+                char separator = input.charAt(2);
+                if (input.startsWith("\\n", 3) && !Character.isDigit(separator)) {
+                    separators.add(separator);
+                    input = input.substring(5);
+                } else {
+                    throw new IllegalArgumentException("커스텀 구분자 정의가 잘못됨");
+                }
+            }
 
+            int value = 0;
             for (int i = 0; i < input.length(); i++) {
                 Character ch = input.charAt(i);
                 if (separators.contains(ch)) {
@@ -27,6 +37,6 @@ public class Application {
             result = result + value;
         }
 
-        System.out.printf("Result: %d%n", result);
+        System.out.printf("결과 : %d%n", result);
     }
 }
