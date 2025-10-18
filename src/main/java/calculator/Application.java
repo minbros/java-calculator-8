9395c30768cbee89f8efd8ed03e7b0ca69f1b418
minbros <pristo24631@gmail.com>
2025-10-18 22:22:11 +0900

    refactor: modify an input message

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Enter the string you want to add.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         if (input.isEmpty()) {
