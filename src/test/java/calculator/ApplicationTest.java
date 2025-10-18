2b03d8cba164a15d62dc1fb01f05707715d99f63
minbros <pristo24631@gmail.com>
2025-10-18 22:25:51 +0900

    test: materialize test code

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,26 +1,40 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 커스텀_구분자_사용() {
+    @ParameterizedTest
+    @CsvSource({
+            "//;\\n1;2, 3",
+            "'1,2:3', 6"
+    })
+    void 계산_테스트(String input, int expectedResult) {
         assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            run(input);
+            assertThat(output()).contains(String.format("결과 : %d", expectedResult));
         });
     }
 
-    @Test
-    void 예외_테스트() {
+    @ParameterizedTest
+    @CsvSource({
+            "//;1, '커스텀 구분자 정의가 잘못되었습니다.'",
+            "//0\\n102, '커스텀 구분자 정의가 잘못되었습니다.'",
+            "'1,2,a', '문자열은 숫자로 끝나야 합니다.'",
+            "'1::2', '구분자가 두 개 연속으로 사용될 수 없습니다.'",
+            "'-1,2:3', '음수는 사용할 수 없습니다.'",
+            "'1,%,2', '문자열은 양수와 구분자로만 구성되어야 합니다.'"
+    })
+    void 예외_테스트(String input, String expectedMessage) {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(expectedMessage)
         );
     }
 
