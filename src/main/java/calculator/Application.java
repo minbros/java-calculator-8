1850d87db2a0e52fc35ef0c1c4aaaf5d0e705675
minbros <pristo24631@gmail.com>
2025-10-18 21:34:25 +0900

    style: change conditional statement structure

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,33 +10,39 @@ public class Application {
         System.out.println("Enter the string you want to add.");
         String input = Console.readLine();
 
-        int result = 0;
-        if (!input.isBlank()) {
-            Set<Character> separators = new HashSet<>(Set.of(',', ':'));
-            if (input.startsWith("//")) {
-                char separator = input.charAt(2);
-                if (input.startsWith("\\n", 3) && !Character.isDigit(separator)) {
-                    separators.add(separator);
-                    input = input.substring(5);
-                } else {
-                    throw new IllegalArgumentException("커스텀 구분자 정의가 잘못됨");
-                }
-            }
-
-            int value = 0;
-            for (int i = 0; i < input.length(); i++) {
-                Character ch = input.charAt(i);
-                if (separators.contains(ch)) {
-                    result = result + value;
-                    value = 0;
-                } else {
-                    value = value * 10 + Integer.parseInt(String.valueOf(ch));
-                }
-            }
-
-            result = result + value;
+        if (input.isEmpty()) {
+            System.out.println("결과 : 0");
+            return;
         }
 
+        int result = 0;
+        if (!Character.isDigit(input.charAt(input.length() - 1))) {
+            throw new IllegalArgumentException("문자열은 숫자로 끝나야 합니다.");
+        }
+
+        Set<Character> separators = new HashSet<>(Set.of(',', ':'));
+        if (input.startsWith("//")) {
+            char separator = input.charAt(2);
+            if (input.startsWith("\\n", 3) && !Character.isDigit(separator)) {
+                separators.add(separator);
+                input = input.substring(5);
+            } else {
+                throw new IllegalArgumentException("커스텀 구분자 정의가 잘못됨");
+            }
+        }
+
+        int value = 0;
+        for (int i = 0; i < input.length(); i++) {
+            Character ch = input.charAt(i);
+            if (separators.contains(ch)) {
+                result = result + value;
+                value = 0;
+            } else {
+                value = value * 10 + Integer.parseInt(String.valueOf(ch));
+            }
+        }
+
+        result = result + value;
         System.out.printf("결과 : %d%n", result);
     }
 }
